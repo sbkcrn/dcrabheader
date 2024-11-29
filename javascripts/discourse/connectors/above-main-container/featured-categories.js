08d4adbdc5580d30e49177b3bfd19b64e81d79f3
sbkcrn <simonbalmain@rabbit.tech>
2024-11-29 21:19:10 +0000

    Add files via upload

---
+++ javascripts/discourse/connectors/above-main-container/featured-categories.js
@@ -0,0 +1,31 @@
+
+import { apiInitializer } from "discourse/lib/api";
+
+export default apiInitializer("0.8", (api) => {
+  const Category = require("discourse/models/category").default;
+
+  api.registerConnectorClass("above-main-container", "featured-categories", {
+    setupComponent(args, component) {
+      api.onPageChange((url, title) => {
+        if (url === "/" || url === "/latest") {
+          document.documentElement.classList.add("custom-featured-categories");
+
+          const definedFeaturedCategories = ["uncategorized", "blog", "two"];
+          const featuredCategories = [];
+
+          const categories = Category.list();
+          for (let cat of categories) {
+            if (definedFeaturedCategories.includes(cat.name.toLowerCase())) {
+              featuredCategories.push(cat);
+            }
+          }
+
+          component.set("featuredCategories", featuredCategories);
+        } else {
+          document.documentElement.classList.remove("custom-featured-categories");
+          component.set("featuredCategories", []);
+        }
+      });
+    },
+  });
+});
